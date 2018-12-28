0ada012c76a4ffa9437e058136091d3bb0f9353e
Chelsey McKinney <chelseymckinney@gmail.com>
2018-12-28 13:44:47 -0600

    found font for main game, and updated text in js file

--- js/script.js
+++ js/script.js
@@ -309,124 +309,20 @@ var quiz_questions = {
 			"answer": 4,
 			"answer-exp": "Spike Lee got inspiration after watching an episode of 'Alfred Hitchcock Presents, in which the main character discuss how hot weather creates violent tendencies."
 	},
+	//Keeping Question #12 as a reference for question syntax of game.
 	// 12: {
-	// 		//"icon": "fighter-jet",
+	// 		//"icon": "",
 	// 		"question": "",
 	// 		"options": {
-	// 				1: "a steam-powered train",
-	// 				2: "an aircraft carrier",
-	// 				3: "a passenger airplane",
-	// 				4: "a battleship",
-	// 				5: "a zeppelin"
+	// 				1: "",
+	// 				2: "",
+	// 				3: "",
+	// 				4: "",
+	// 				5: ""
 	// 		},
 	// 		"answer": 2,
-	// 		"answer-exp": "In The Final Countdown (1980), a modern aircraft carrier encounters a strange storm while out on a training mission. Entering the storm transports the carrier through time from 1980 to 1941, arriving the day before the attack on Pearl Harbor. The ships's captain must then decide whether to try to prevent the attack and change history."
+	// 		"answer-exp": "."
 	// },
-	// 13: {
-	// 		"icon": "history",
-	// 		"question": "In Michael Crichton's 1999 book <strong>Timeline</strong>, the characters travel to which period in time?",
-	// 		"options": {
-	// 			 1: "13th Century France",
-	// 				2: "15th Century Spain",
-	// 				3: "14th Century Scotland",
-	// 				4: "13th Century Germany",
-	// 				5: "14th Century France"
-	// 		},
-	// 		"answer": 5,
-	// 	 "answer-exp": "The story in Timeline involves a group of history students who travel to 14th Century France to rescue their professor."
-	// },
-	// 14: {
-	// 		"icon": "history",
-	// 		"question": "Which television show included an episode in which a character goes back in time and saves Dr. Heimlich from choking - giving the doctor the idea for the Heimlich maneuver?",
-	// 		"options": {
-	// 				1: "Sliders (1995 &ndash; 2000)",
-	// 				2: "Journeyman (2007)",
-	// 				3: "Timecop (1997)",
-	// 				4: "Quantum Leap (1989 &ndash; 1993)",
-	// 				5: "The Time Tunnel (1966 &ndash; 1967)"
-	// 		},
-	// 		"answer": 4,
-	// 		"answer-exp": "In \"Thou Shall Not ...\", a 1989 episode of Quantum Leap (1989 - 1993), the main character (Sam) performs the Heimlich Maneuver on a choking man, who is later revealed to be Henry Heimlich - the inventor of the Heimlich Maneuver."
-	// },
-	// 15: {
-	// 		"icon": "child",
-	// 		"question": "Which animated television show, featured time travel adventures, an anthropomorphic dog, and a girl from the future named Cupcake?",
-	// 		"options": {
-	// 				1: "The Fonz and the Happy Days Gang (1980 &ndash; 1982)",
-	// 				2: "Flint the Time Detective (1998 &ndash; 1999)",
-	// 				3: "Bill &amp; Ted's Excellant Adventure (1990)",
-	// 				4: "Gadget Boy &amp; Heather (1995)",
-	// 				5: "Samurai Jack (2001 &ndash; 2004)"
-	// 		},
-	// 		"answer": 1,
-	// 		"answer-exp": "The Fonz and the Happy Days Gang (1980 - 1982) was a Hanna-Barbera animated television series. It was a spin-off of Happy Days and included three of its characters, The Fonz (Henry Winkler), Richie Cunningham (Ron Howard), and Ralph Malph (Donny Most). These characters were joined by an anthropomorphic dog, Mr. Cool, and a girl from the future, Cupcake. The group travel through history in a time machine, trying to get back to 1957 Milwaukee."
-	// },
-	// 16: {
-	// 		"icon": "train",
-	// 		"question": "<strong>Time Express (1979)</strong> was a television show about a train that took people on trips through time. Who starred in the show?",
-	// 		"options": {
-	// 				1: "William Shatner",
-	// 				2: "Alan Alda",
-	// 				3: "Vincent Price",
-	// 				4: "Henry Winkler",
-	// 				5: "Danny DeVito"
-	// 		},
-	// 		"answer": 3,
-	// 		"answer-exp": "Time Express was a short-lived television anthology series that was a time travel version of similar anothology shows like Love Boat or Fantasy Island. The train's husband (Vincent Price) and wife hosts would shepard its passengers back in time to relive a crucial point in their lives."
-	// },
-	// 17: {
-	// 		"icon": "rocket",
-	// 		"question": "In the <strong>Futurama (1999 - 2013)</strong> episode \"Roswell That Ends Well\" the main character, Fry, sleeps with his grandmother and ends up becoming his own grandfather. Time travel that results in someone becoming their own ancestor is an example of what concept?",
-	// 		"options": {
-	// 				1: "the butterfly effect",
-	// 				2: "a predestination paradox",
-	// 				3: "temporal merging",
-	// 				4: "quantum entanglement",
-	// 				5: "multiple universe theory"
-	// 		},
-	// 		"answer": 2,
-	// 		"answer-exp": "A predestination paradox (also known as a causality loop) is when causation between two or more events form a loop. For example, Fry being his own ancestor is a cause of his own existence, but his existence is required for him to time travel and become his own ancestor."
-	// },
-	// 18: {
-	// 		"icon": "newspaper-o",
-	// 		"question": "Which writer was editor of The Sun newspaper and is often credited with being the first to use several science fiction ideas, including time travel in his short stories?",
-	// 		"options": {
-	// 				1: "Charles Anderson Dana",
-	// 				2: "Mark Twain",
-	// 				3: "Edward Page Mitchell",
-	// 				4: "Charles Dickens",
-	// 				5: "Herbert George Wells"
-	// 		},
-	// 		"answer": 3,
-	// 		"answer-exp": "Edward Page Mitchell (1852 - 1927) was a short story writer and editor of a New York newspaper. Mitchell's stories incorporated a number of science fiction concepts before they were used in other works, including stories involving an invisible man in The Crystal Man (1881), time travel in The Clock that Went Backward (1881), faster-than-light travel in The Tachypomp (1874), a thinking computer and a cyborg in The Ablest Man in the World (1879), teleportation in The Man without a Body (1877), and mind transfer in Exchanging Their Souls (1877)."
-	// },
-	// 19: {
-	// 		"icon": "file-text-o",
-	// 		"question": "Who coined the term \"time machine\" to describe a mechanism used to transport a traveller through time?",
-	// 		"options": {
-	// 				1: "Jules Verne",
-	// 				2: "Mark Twain",
-	// 				3: "William Shakespere",
-	// 				4: "Washington Irving",
-	// 				5: "Herbert George Wells"
-	// 		},
-	// 		"answer": 5,
-	// 		"answer-exp": "H.G. WELLS (1866 - 1946) was the author of The Time Machine (1895), in which a scientist in Victorian England builds a machine to travel through time. Wells is credited with coining the term \"time machine\" in this work."
 
-	// },
-	// 20: {
-	// 		"icon": "newspaper-o",
-	// 		"question": "What was the name of the television series (1996 - 2000) in which the main character receives a newspaper from one day in the future every day?",
-	// 		"options": {
-	// 				1: "Tomorrow's Tales",
-	// 				2: "The Paradox Papers",
-	// 				3: "Next Day News",
-	// 				4: "The Day to Come",
-	// 				5: "Early Edition"
-	// 		},
-	// 		"answer": 5,
-	// 		"answer-exp": "Early Edition (1996 - 2000) followed the adventures of Gary Hobson (Kyle Chandler) who mysteriously receives the Chicago Sun-Times newspaper the day before it is actually published, and who uses this knowledge to prevent terrible events contained in its pages."
-
-	//}
 };
 
